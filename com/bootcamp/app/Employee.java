2c8f6b0a6bf351a8d39360e444d122e0baaec4db
blackhathacker101 <justindouglas1738@gmail.com>
2021-09-29 13:47:14 -0300

    first commit

---
+++ com/bootcamp/app/Employee.java
@@ -0,0 +1,52 @@
+package com.bootcamp.app;
+
+public abstract class Employee {
+
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(int employeeId, String firstName, String lastName) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public abstract double calculateSalary();
+	
+	@Override
+	public String toString() {
+		
+		return String.format("%-20s-%d%n", "Employee ID:" + employeeId) + String.format("%-20s-", "%s%n", "Employee Name:" + firstName + " " + lastName);
+		
+	}
+}
---
+++ com/bootcamp/app/HourlyEmployee.java
@@ -0,0 +1,15 @@
+package com.bootcamp.app;
+
+public class HourlyEmployee extends Employee {
+
+	public double calculateSalary() {
+		return 0;
+		
+	};
+
+	@Override
+	public String toString() {
+
+		return super.toString();
+	}
+}
